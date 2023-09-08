bc8329b0aac5c4f14ebb94a14880c7650e97fc9d
Mikhail <michael.leonov96@mail.ru>
2023-09-08 12:37:47 +0300

    feat: add serve static for announcement images

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { DatabaseModule } from "./common/config/database.module";
 import { UsersModule } from "./modules/users/users.module";
 import { RolesModule } from "./modules/roles/roles.module";
 import { AuthModule } from "./modules/auth/auth.module";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from "path";
 
 @Module({
   imports: [
@@ -12,6 +14,9 @@ import { AuthModule } from "./modules/auth/auth.module";
     UsersModule,
     RolesModule,
     AuthModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "..", "src", "static", "uploads"),
+    }),
   ],
 })
 export class AppModule {}
